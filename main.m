43a344469380fbf8deee63128e15b614484e79ad
snamiki1212 <snamiki1212@gmail.com>
2021-07-12 11:29:47 -0700

    Update main

--- main.m
+++ main.m
@@ -10,10 +10,20 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-        NSString *msg = [@"hShPsText" stringByPigLatinization];
-        NSLog(@"%@", msg);
+        NSString *text1 = @"Pig";
+        NSString *text2 = @"Latin";
+        NSString *text3 = @"China";
+        NSString *text4 = @"SmallStation";
+        NSString *text5 = @"GlGlGoogle";
+        NSString *text6 = @"ChShSm";
+        
+        NSLog(@"%@ => %@", text1, [text1 stringByPigLatinization]);
+        NSLog(@"%@ => %@", text2, [text2 stringByPigLatinization]);
+        NSLog(@"%@ => %@", text3, [text3 stringByPigLatinization]);
+        NSLog(@"%@ => %@", text4, [text4 stringByPigLatinization]);
+        NSLog(@"%@ => %@", text5, [text5 stringByPigLatinization]);
+        NSLog(@"%@ => %@", text6, [text6 stringByPigLatinization]);
+
     }
     return 0;
 }
